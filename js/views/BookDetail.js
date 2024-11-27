e33cb05ad4bebd7a652f9e6d7d47c514b46cabb1
Rufsan Hossain Santo <rufsanhossain315@gmail.com>
2024-11-27 22:57:00 +0600

    underscore template

--- js/views/BookDetail.js
+++ js/views/BookDetail.js
@@ -1,7 +1,11 @@
 app.views.BookDetail = Backbone.View.extend({
+
+    template: _.template($('script[data-id=book-detail-view]').html()),
+
     initialize: function () {
         this.listenTo(this.model, "change", this.render);
     },
+
     render: function () {
 
         var info = this.model.get("volumeInfo");
@@ -13,24 +17,11 @@ app.views.BookDetail = Backbone.View.extend({
             publisher = info.publisher + ' - Publisher';
         }
 
-        this.$el.html(
-            '<header class="book-header content content-constrained row">' +
-            '<div class="column thumb-image">' +
-            '<img src="' + (images.small || images.thumbnail) + '" alt="">' +
-            '</div>' +
-            '<div class="column l-pad">' +
-            '<div class="title">' + info.title + '</div>' +
-            '<div>' +
-            (info.authors ? '<em>' + info.authors.join(" - ") + '</em>' : "" + info.publishedDate) + ' <br>' +
-            publisher +
-            '</div>' +
-            '</div>' +
-            '</header>' +
-            '<div class="content book-content content-constrained standout">' +
-            '<h1 class="title">Description</h1>' +
-            info.description +
-            '</div >'
-        );
+        this.$el.html(this.template({
+            info: info,
+            publisher: publisher,
+            images: images
+        }));
 
         return this;
     }
